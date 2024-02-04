f23e7280dac71f277ff3d048cbf969cda7eaabf7
njsaugat <njsaugat@gmail.com>
2024-02-04 07:50:00 +0545

    models

--- accounts/models.py
+++ accounts/models.py
@@ -84,9 +84,7 @@ class User(AbstractBaseUser,CreatedModified):
 
 class ForgotPassword(models.Model):
     
-    otp=models.IntegerField(validators=[
-        models.validators.MinValueValidator(limit_value=0),
-    ],null=False)
+    otp=models.IntegerField(null=False)
     
     created_at=models.DateTimeField(auto_now_add=True)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
