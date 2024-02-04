e680427b3279074e17e42dc048d8ea50edd6df5e
njsaugat <njsaugat@gmail.com>
2024-02-04 07:50:28 +0545

    serialize

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,13 +1,17 @@
-from rest_framework import serializers
 from django.utils.encoding import smart_str,force_bytes,DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import timezone
 from django.conf import settings
 from django.urls import reverse
+
+from rest_framework import serializers
+# from rest_framework.validators import MinValueValidator
+
 from decouple import config
 from accounts.models import User,ForgotPassword
 from accounts.utils import Util
+from datetime import timedelta,datetime
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password=serializers.CharField(style={'input_type':'password'},write_only=True)
     
@@ -67,22 +71,30 @@ class UserChangePasswordSerializer(serializers.Serializer):
         user.save()
         return attrs
     
-
-class SendPasswordResetEmailSerializer(serializers.Serializer):
-    email=serializers.EmailField(max_length=255)
+    
+class EmailSerializer(serializers.Serializer):
+    email=serializers.EmailField(max_length=255)   
 
     class Meta:
         fields=['email']
-
-    
-    def validate(self,attrs):
+      
+    def validate(self, attrs):
         email=attrs.get('email')
 
         if not User.objects.filter(email=email).exists():
-            raise serializers.ValidationError("You aren't registered with that email.")
+            raise serializers.ValidationError("The email isn't registered.")
         
+        return attrs  
+
+class SendPasswordResetEmailSerializer(EmailSerializer):
+
+    
+    def validate(self,attrs):
+        super().validate(attrs)
+        email=attrs.get('email')
+
         user=User.objects.get(email=email)
-        generated_otp=Util.generate_uid()[:6]
+        generated_otp=Util.generate_otp()
         
         new_otp=ForgotPassword.objects.create(otp=generated_otp,user=user)
         new_otp.save()
@@ -92,7 +104,11 @@ class SendPasswordResetEmailSerializer(serializers.Serializer):
 
         link=config("BASE_URL")+ reverse('reset-password', args=[uid, token])
 
-        body='Click following link to reset your password. \n'+ link + generated_otp
+        body=f"""Click following link to reset your password. 
+        
+        {link}  
+        
+        The generated otp is <b> {str(generated_otp)} </b> """
 
         data={
             'subject':'Reset your password',
@@ -135,6 +151,9 @@ class UserPasswordResetSerializer(serializers.Serializer):
         
         
         
+class OTPSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields='__all__'
 
     
 class UserOTPSerializer(serializers.Serializer):
@@ -143,13 +162,14 @@ class UserOTPSerializer(serializers.Serializer):
     
     def validate(self, validated_data):
         otp=validated_data.get('otp')
-        existing_otp=ForgotPassword.objects.filter(otp=otp)
-        if (not existing_otp.exists() or otp!=existing_otp):
+        existing_otp=ForgotPassword.objects.filter(otp=otp).first()
+
+        if (not existing_otp or otp!=existing_otp.otp):
             raise serializers.ValidationError("The otp you entered is invalid.")
 
         timeout_duration=getattr(settings,'PASSWORD_RESET_TIMEOUT',900)
 
-        if existing_otp.created_at+timeout_duration >=timezone.now():
+        if existing_otp.created_at + timedelta(seconds=timeout_duration) < timezone.now():
             raise serializers.ValidationError("The otp is already expired.")
         
         return validated_data
